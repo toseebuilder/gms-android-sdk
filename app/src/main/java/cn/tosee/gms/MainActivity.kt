6a9844fe1aafac0650868e87d555d1de4ba0791d
lizejun <zejun.li@tosee.cn>
2022-01-10 18:05:08 +0800

    更新ts

--- app/src/main/java/cn/tosee/gms/MainActivity.kt
+++ app/src/main/java/cn/tosee/gms/MainActivity.kt
@@ -24,63 +24,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val instance: GMSClient = GMSClient.createInstance(this, "7zVD31N8WqmP0xyE", Options(
-            EnvConfig("https://api-dev.rzrtc.com")
-        ), object :
-            GMSClientListener {
-            override fun onConnectionStateChanged(code: Int, reason: Int) {
-                Log.e(TAG, "onConnectionStateChanged: $code reason $reason")
-            }
-
-            override fun onMessageReceived(message: GMSMessage?, userId: String) {
-                Log.e(TAG, "onMessageReceived: $message userId $userId")
-            }
-
-            override fun onPeersOnlineStatusChanged(peersStatus: Map<String, Int>) {
-                Log.e(TAG, "onPeersOnlineStatusChanged: $peersStatus ")
-            }
-
-            override fun onPeersUserAttributesChanged(userAttributes: Map<String, List<GMSAttributeWithState>>) {
-                Log.e(TAG, "onPeersUserAttributesChanged: $userAttributes ")
-            }
-
-            override fun onTokenExpired() {
-                Log.e(TAG, "onTokenExpired:")
-            }
-        })
-        val timestamp = System.currentTimeMillis()
-        val token =
-            createToke("7zVD31N8WqmP0xyE", "1113", timestamp, "e6cd8cec107a4b5abee9cfae52f15457")
-        instance.login(LoginParams(token!!, "1113", timestamp), object : ResultCallback<Void> {
-
-            override fun onFailure(errorInfo: ErrorInfo) {
-                Log.e(TAG, "login onFailure:")
-
-            }
-
-            override fun onSuccess(responseInfo: Void?) {
-                Log.e(TAG, " login onSuccess:")
-
-            }
-        })
-
-
     }
 
-    fun createToke(appId: String?, userId: String?, timestamp: Long, appkey: String?): String? {
-        if (EmptyUtils.isEmpty(appId)) {
-            return null
-        }
-        if (EmptyUtils.isEmpty(userId)) {
-            return null
-        }
-        if (EmptyUtils.isEmpty(appkey)) {
-            return null
-        }
-        val params = StringBuffer("appId=").append(appId).append("&timestamp=").append(timestamp)
-            .append("&userId=")
-            .append(userId).append(appkey).toString()
-        return EncryptUtils.MD5(params)
-    }
 }
