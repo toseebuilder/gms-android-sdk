e7e0dd0db8ed3a9191523af27c3261dcc0ff14cd
gaoerhan <erhan.gao@tosee.cn>
2022-03-16 15:52:30 +0800

    feat:2.1.0:修改包名

--- app/src/main/java/cn/tosee/gms/MainActivity.kt
+++ app/src/main/java/cn/tosee/gms/MainActivity.kt
@@ -1,19 +1,7 @@
 package cn.tosee.gms
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import cn.ts.gms.ErrorInfo
-import cn.ts.gms.ResultCallback
-import cn.ts.gms.bean.LoginParams
-import cn.ts.gms.client.GMSClient
-import cn.ts.gms.client.GMSClientListener
-import cn.ts.gms.connect.bean.EnvConfig
-import cn.ts.gms.connect.bean.GMSMessage
-import cn.ts.gms.connect.bean.Options
-import cn.ts.gms.user.GMSAttributeWithState
-import cn.ts.gms.utils.EmptyUtils
-import cn.ts.gms.utils.EncryptUtils
 
 class MainActivity : AppCompatActivity() {
 
